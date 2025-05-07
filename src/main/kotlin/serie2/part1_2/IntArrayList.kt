d0935faf0f767993e7bc6844cf61a35b800e4069
biascccarvalho <beatrizsjcc06@gmail.com>
2025-05-07 12:43:08 +0100

    intarray

--- src/main/kotlin/serie2/part1_2/IntArrayList.kt
+++ src/main/kotlin/serie2/part1_2/IntArrayList.kt
@@ -1,24 +1,35 @@
 package serie2.part1_2
 
-class IntArrayList(capacity: Int) : Iterable <Int> {
+class IntArrayList(val capacity: Int) /*  : Iterable <Int> */ {
+    val intList = IntArray(capacity)
+    var size = 0
+    var tail = 0
+    var head = 0
 
-    fun append(x:Int):Boolean {
-        TODO("Not yet implemented")
+    fun append(x: Int): Boolean {
+        if (size == capacity) return false
+        intList[tail] = x
+        tail = (tail + 1) % capacity
+        size++
+        return true
     }
 
-    fun get(n:Int):Int?  {
-        TODO("Not yet implemented")
+    fun get(n: Int): Int? {
+        if (n !in 0 until size) return null
+        return intList[(head + n) % capacity]
     }
 
-    fun addToAll(x:Int)   {
-        TODO("Not yet implemented")
+    fun addToAll(x: Int) {
+        for (i in 0 until size) {
+            val index = (head + i) % capacity
+            intList[index] += x
+        }
     }
 
-    fun remove():Boolean {
-        TODO("Not yet implemented")
-    }
-
-    override fun iterator(): Iterator<Int> { // Opcional
-        TODO("Not yet implemented")
+    fun remove(): Boolean {
+        if (size == 0) return false
+        head = (head + 1) % capacity
+        size--
+        return true
     }
 }
