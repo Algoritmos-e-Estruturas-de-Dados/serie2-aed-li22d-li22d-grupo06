0411890ce8b472448fbe7eddba66a02b6eeec302
Peres <davidcisneirosperes@gmail.com>
2025-05-07 11:04:27 +0100

    Added 1 and fixed HM test and IAL parameter

--- src/main/kotlin/serie2/part1_2/arrayUtils.kt
+++ src/main/kotlin/serie2/part1_2/arrayUtils.kt
@@ -2,5 +2,14 @@ package serie2.part1_2
 import kotlin.random.Random
 
 fun minimum(maxHeap: Array<Int>, heapSize: Int): Int {
-    TODO("Not yet implemented")
+    if (heapSize == 0) throw Exception("Heap is empty")
+    if (heapSize == 1) return maxHeap[0]
+    val mid = heapSize / 2
+    var min = maxHeap[mid]
+    for (i in mid until heapSize) {
+        if (maxHeap[i] < min) {
+            min = maxHeap[i]
+        }
+    }
+    return min
 }
